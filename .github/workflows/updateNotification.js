10a69811dc4698ff7fd998fdc5df51f8d09f51d9
abose <arun@core.ai>
2023-07-07 16:26:03 +0530

    feat: add github actions get release details

--- .github/workflows/updateNotification.js
+++ .github/workflows/updateNotification.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 export default async function printStuff({github, context}) {
     console.log(github, context, "yo");
     const fullRepoName = context.payload.repository.full_name;
@@ -6,16 +8,21 @@ export default async function printStuff({github, context}) {
     console.log("repository owner, repo: ", owner, repo); // Eg: 'phcode-dev/phoenix-desktop'
     const releaseTag = context.payload.release.tag_name;
     console.log("Release Tag name: ", releaseTag);
+    const isPreRelease = context.payload.release.prerelease;
+    console.log("Release isPreRelease: ", isPreRelease);
     const releaseNotes = context.payload.release.body;
     console.log("Release Notes: ", releaseNotes);
     const releaseID = context.payload.release.id;
-    const releaseAssets = await github.rest.repos.listReleaseAssets({
+    const releaseAssets = (await github.rest.repos.listReleaseAssets({
         owner,
         repo,
         release_id: releaseID,
-    });
+    })).data;
     console.log("Release assets: ", releaseAssets);
 
     // you can call additional github apis using await github.rest.* apis
     // See https://octokit.github.io/rest.js/v19#repos-get-release-by-tag for more availableapis
+
+    const githubWorkspaceRoot = github.workspace;
+    fs.writeFileSync(`${githubWorkspaceRoot}/docs/tauri/release.json`, JSON.stringify(releaseAssets, null, 2));
 }
