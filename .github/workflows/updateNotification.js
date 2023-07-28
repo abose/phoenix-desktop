8543702179bad757d6e0c45373806ed8d0508c1f
abose <arun@core.ai>
2023-07-28 12:03:58 +0530

    test

--- .github/workflows/updateNotification.js
+++ .github/workflows/updateNotification.js
@@ -1,4 +1,29 @@
 import fs from 'fs';
+import {PRODUCT_NAME_SUFFIX_FOR_STAGE, UPDATE_NOTIFICATION_LATEST_JSON_FILE_PATH} from "../../src-build/constants.js";
+
+function _makePrefix(name) {
+    return name.trim().split(" ").join(".");
+}
+
+// these are the prefix for names of assets in the release created by tauri GitHub action.
+// Eg. Phoenix.Code.Experimental.Build_3.2.2_aarch64.dmg, Phoenix.Code.Pre-release_3.2.2_x64-setup.exe, etc..
+// note that not all platform bin names follow these convention, so we just check if any of the assets follow the pattern
+const DEV_STAGE_PRODUCT_NAME_PREFIX = "Phoenix.Code." + _makePrefix(PRODUCT_NAME_SUFFIX_FOR_STAGE.dev);
+const PRE_RELEASE_STAGE_PRODUCT_NAME_PREFIX = "Phoenix.Code." + _makePrefix(PRODUCT_NAME_SUFFIX_FOR_STAGE.stage);
+//"": "production" has no suffix
+
+// identify if the release is staging/pre-release/prod, then based on that get the pathname of the update json file
+function _identifyUpdateJSONPath(releaseAssets) {
+    for(let releaseAsset of releaseAssets) {
+        if(releaseAsset.name.startsWith(DEV_STAGE_PRODUCT_NAME_PREFIX)){
+            return UPDATE_NOTIFICATION_LATEST_JSON_FILE_PATH.dev;
+        }
+        else if(releaseAsset.name.startsWith(PRE_RELEASE_STAGE_PRODUCT_NAME_PREFIX)){
+            return UPDATE_NOTIFICATION_LATEST_JSON_FILE_PATH.stage;
+        }
+    }
+    return UPDATE_NOTIFICATION_LATEST_JSON_FILE_PATH.production;
+}
 
 export default async function printStuff({github, context, githubWorkspaceRoot}) {
     console.log(github, context, "yo");
@@ -24,5 +49,6 @@ export default async function printStuff({github, context, githubWorkspaceRoot})
     // See https://octokit.github.io/rest.js/v19#repos-get-release-by-tag for more availableapis
 
     // write to the docs folder here. all changes made here to the docs folder will be part of the pull request
-    fs.writeFileSync(`${githubWorkspaceRoot}/docs/tauri/release.json`, JSON.stringify(releaseAssets, null, 2));
+    console.log("Updating tauri update JSON file: ", _identifyUpdateJSONPath(releaseAssets));
+    fs.writeFileSync(`${githubWorkspaceRoot}/${_identifyUpdateJSONPath(releaseAssets)}`, JSON.stringify(releaseAssets, null, 2));
 }
